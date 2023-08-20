123775a068788477d4906b19b2baa368a44beb9f
Roberto Vieira Rezende <robertovrd2106@gmail.com>
2023-08-20 18:07:32 +0000

    arquivos ignorados e arquivos adicionados

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home(){
+   return (
+    <div>
+        <h1>salsicha ao sugo, strogonof de frango, batata palha, legumes saute</h1>
+        <h1>frango grelhado, suino grelhado, farofa, batata frita</h1>
+        <h1>arroz carreteiro, linguiça toscana</h1>
+   </div>
+  );
+
+}
