11e04da4cd7c5123ec51b05a197852a85cedc50c
Roberto Vieira Rezende <robertovrd2106@gmail.com>
2023-08-20 18:09:15 +0000

    mend

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 export default function Home(){
    return (
     <div>
-        <h1>salsicha ao sugo, strogonof de frango, batata palha, legumes saute</h1>
-        <h1>frango grelhado, suino grelhado, farofa, batata frita</h1>
-        <h1>arroz carreteiro, linguiça toscana</h1>
+        <h1>Café</h1>
    </div>
   );
 
